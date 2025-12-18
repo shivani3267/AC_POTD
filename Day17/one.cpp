d9f4225f5014d0b0ba829f5a551d85b54f56562f
Shivani <shivanikumari9979@gmail.com>
2025-12-18 21:04:09 +0530

    Solutions added

---
+++ Day17/one.cpp
@@ -0,0 +1,27 @@
+//LEETCODE :165. Compare Version numbers
+
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        int m=v1.size(),n=v2.size();
+        int i=0,j=0;
+        while(i<m || j<n){
+            long long temp1=0;
+            while(i<m && v1[i] != '.'){
+                temp1 = temp1*10 + v1[i] - '0';
+                i++;
+            }
+            long long temp2=0;
+            while(j<n && v2[j] != '.'){
+                temp2 = temp2*10 + v2[j] - '0';
+                j++;
+            }
+            if(temp1<temp2) return -1;
+            if(temp2<temp1) return 1;
+            i++;
+            j++;
+        }
+        return 0;
+        
+    }
+};
